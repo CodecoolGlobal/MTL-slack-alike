cbfb2ab56a1c94752c621a8002a8fd51ce4666cd
Liz <pachaffy@gmail.com>
2019-04-10 13:28:57 +0200

    Create constructor for User

--- public/scripts/model/User.js
+++ public/scripts/model/User.js
@@ -1,6 +1,9 @@
 export class User {
     constructor(name) {
-
+        this.name = name;
+        this.id = id;
+        this.isOnline = isOnline;
+        this.activeChannel = activeChannel;
     }
 }
 
