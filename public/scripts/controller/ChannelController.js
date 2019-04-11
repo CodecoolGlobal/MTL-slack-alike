fd78c379a8c45b1e7cf63a7f973faa2f4929f7c8
Ziemowit Błażków <yasha2yashaa@gmail.com>
2019-04-11 15:53:52 +0200

    Add onDisconnect active user deletion

--- public/scripts/controller/ChannelController.js
+++ public/scripts/controller/ChannelController.js
@@ -6,4 +6,14 @@ getCurrentUserPromise().then(function (result) {
     let channelView = new ChannelView(result);
     console.log(channelView.user.id);
     console.log(channelView.user.name);
+
+    // onDisconnect
+
+    let amOnline = firebase.database().ref('.info/connected');
+    let userRef = firebase.database().ref('users/' + channelView.user.id);
+    amOnline.on('value', function(snapshot) {
+        if (snapshot.val()) {
+            userRef.onDisconnect().remove();
+        }
+    });
 });
