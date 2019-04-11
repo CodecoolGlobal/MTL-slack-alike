b59c1232e26e87581e7f944c3fd9241b1d477948
mszotek <mszotek@gmail.com>
2019-04-11 17:04:57 +0200

    Clear members

--- public/scripts/view/OnlineUsersView.js
+++ public/scripts/view/OnlineUsersView.js
@@ -4,17 +4,24 @@ export default class OnlineUsersView {
     }
 
     displayOnlineUsers() {
-        let membersOnline = document.getElementById("members");
+        let membersOnline = document.getElementById("members-list");
         let users = firebase.database().ref("users/");
         let userName;
 
         users.on("value", function (data) {
+            this.clearMembers();
             data.forEach(function (data) {
-                userName = document.createElement("p");
+                userName = document.createElement("li");
                 userName.setAttribute("class", "online");
                 userName.textContent = data.val().name;
                 membersOnline.appendChild(userName);
             });
-        });
+        }.bind(this));
     }
+
+    clearMembers(){
+        document.getElementById('members-list').innerHTML = '';
+    }
+
+
 }
